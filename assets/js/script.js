2aa1aa93dab4bae8d9d127c7189f164b6dc786ed
MamaOsaBit <65994187+MamaOsaBit@users.noreply.github.com>
2024-04-29 20:36:49 -0400

    cambios en formulario
    me estaba dando problemas unas cosas que queria implementar en formulario, como solo fueron 3 dias lo que vi js, no se mucho, despues de mucho intento, se logro

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,19 @@ $(function () {
 });
 
 
+/* Formulario */
+$(".btn-info").click(function(){
+    var mensaje = $("#exampleFormControlTextarea1").val(); // Obtener el valor del campo de mensaje
+    if (mensaje.trim() === "") { // Verificar si el mensaje está vacío después de eliminar los espacios en blanco
+        alert("Por favor, ingrese un mensaje.");
+    } else {
+        alert("El mensaje ha sido enviado.");
+        $("#exampleFormControlInput1").val(""); // Limpiar el campo de nombre
+        $("#exampleFormControlInput2").val(""); // Limpiar el campo de asunto
+        $("#exampleFormControlTextarea1").val(""); // Limpiar el campo de mensaje
+    }
+});
+
+
+
+
